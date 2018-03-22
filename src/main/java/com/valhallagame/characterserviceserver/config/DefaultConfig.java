af9abb055244fbe060d912c9bcb7de854126aa73
Mio Engel <mioengel@gmail.com>
2018-03-22 17:51:11 +0100

    Added development profile

--- src/main/java/com/valhallagame/characterserviceserver/config/DefaultConfig.java
+++ src/main/java/com/valhallagame/characterserviceserver/config/DefaultConfig.java
@@ -4,15 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import com.valhallagame.common.DefaultServicePortMappings;
 import com.valhallagame.wardrobeserviceclient.WardrobeServiceClient;
 
 @Configuration
 @Profile("default")
-public class DevConfig {
+public class DefaultConfig {
 	@Bean
 	public WardrobeServiceClient wardrobeServiceClient() {
-		WardrobeServiceClient.init("http://dev-wardrobe-service:" + DefaultServicePortMappings.WARDROBE_SERVICE_PORT);
 		return WardrobeServiceClient.get();
 	}
 }
